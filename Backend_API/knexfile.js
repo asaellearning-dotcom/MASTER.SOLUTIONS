5ff64cf14814ba9b0ee528e9412727fdb08152c5
Asael De La Rosa <asael.learning@gmail.com>
2026-08-11 05:01:32 -0500

    logs important data

--- Backend_API/knexfile.js
+++ Backend_API/knexfile.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 
+console.log('process.env.DB_HOST', process.env.DB_HOST);
+console.log('process.env.DB_PORT', process.env.DB_PORT);
+console.log('process.env.DB_USER', process.env.DB_USER);
+console.log('process.env.DB_PASSWORD', process.env.DB_PASSWORD);
+console.log('process.env.DB_NAME', process.env.DB_NAME);
+
 const knex = require('knex')({
   client: 'mysql2',
   connection: {
